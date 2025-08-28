a08f0a3546e21a27b4327ff0c6a66b7dde476f67
sarathsai1 <150181173+sarathsai1@users.noreply.github.com>
2025-08-28 19:21:58 +0530

    playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ reporter: [
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-     headless: false,
+     headless: true,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
         video: 'on-first-retry',
